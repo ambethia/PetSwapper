9bf7c0a84ea526a8dd3aba58dd1af2922c7d1518
Jason L Perry <jasper@ambethia.com>
2013-10-16 13:46:22 -0400

    Health check

--- PetSwapper.lua
+++ PetSwapper.lua
@@ -1,13 +1,10 @@
 local defaults, db = {
-  isEnabled    = true,
-  swapMin      = 1,
-  swapMax      = 25,
-  slotOne      = true,
-  slotTwo      = true,
-  slotThree    = false,
-  slotOneFix   = nil,
-  slotTwoFix   = nil,
-  slotThreeFix = nil,
+  isEnabled     = true,
+  swapMin    = 1,
+  swapMax    = 25,
+  slotOne    = true,
+  slotTwo    = true,
+  slotThree  = false
 }
 
 local frame, swapping = CreateFrame("Frame"), false
@@ -44,11 +41,13 @@ function frame:PET_JOURNAL_LIST_UPDATE()
       local petGUID, ability1, ability2, ability3, locked = C_PetJournal.GetPetLoadOutInfo(slot)
       if petGUID then
         local _, _, level, _, _, _, _, name = C_PetJournal.GetPetInfoByPetID(petGUID)
-        if level < db.swapMin or level >= db.swapMax and ((slot == 1 and db.slotOne) or (slot == 2 and db.slotTwo) or (slot == 3 and db.slotThree)) then
+        local health = C_PetJournal.GetPetStats(petGUID)
+        if (level < db.swapMin or level >= db.swapMax or health < 1) and ((slot == 1 and db.slotOne) or (slot == 2 and db.slotTwo) or (slot == 3 and db.slotThree)) then
           for index = 1, select(2, C_PetJournal.GetNumPets()) do
             if not swapped[slot] then
               local _petGUID, _, _, _, _level, _favorite, _, _speciesName = C_PetJournal.GetPetInfoByIndex(index)
-              if _favorite and _level >= db.swapMin and _level < db.swapMax then
+              local _health = C_PetJournal.GetPetStats(_petGUID)
+              if _favorite and _level >= db.swapMin and _level < db.swapMax and _health > 0 then
                 if _petGUID ~= C_PetJournal.GetPetLoadOutInfo(1) and
                    _petGUID ~= C_PetJournal.GetPetLoadOutInfo(2) and
                    _petGUID ~= C_PetJournal.GetPetLoadOutInfo(3) and not locked then
@@ -62,9 +61,6 @@ function frame:PET_JOURNAL_LIST_UPDATE()
         end
       end
     end
-    if not db.slotOne   and db.slotOneFix   then C_PetJournal.SetPetLoadOutInfo(1, db.slotOneFix)   end
-    if not db.slotTwo   and db.slotTwoFix   then C_PetJournal.SetPetLoadOutInfo(2, db.slotTwoFix)   end
-    if not db.slotThree and db.slotThreeFix then C_PetJournal.SetPetLoadOutInfo(3, db.slotThreeFix) end
     swapping = false
   end
 end
